7888beabb1155dedd35a3929cf37230b3c776787
tanmay <tanmaypaye@gmail.com>
2022-08-06 12:13:16 +0530

    vhjvuyfyyuyg/lkj

---
+++ page3.py
@@ -0,0 +1,17 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Tanmay:Password@tanmay.si14ffa.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+database = client['myinfo']
+collection = database['tan']
+
+# record = collection.find()
+# for i in record:
+#     print(i)
+
+# data = collection.find({'sub': 'data science'})
+# for i in data :
+#     print(i)
+
+data = collection.find({'batteryVoltageStableTime': {'$gt': 0}})
+for i in data:
+    print(i)
