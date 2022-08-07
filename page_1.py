2683d52be1948b977e15caf16f286f6316d334ca
tanmay <tanmaypaye@gmail.com>
2022-08-07 10:07:49 +0530

    1st

--- page_1.py
+++ page_1.py
@@ -1,10 +1,11 @@
-hfdsahiu
-sfsdjfio
-ds'fhjdsf' \
-<<<<<<<<< Temporary merge branch 1
-  ''
-=========
-  ''hju
-yu
-fuyfuy
->>>>>>>>> Temporary merge branch 2
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Tanmay:Password@tanmay.si14ffa.mongodb.net/?retryWrites=true&w=majority")
+mydb = client.test
+
+data01 = {'l': 'q', 'k': 'w', 'j': 'e', 'h': 'r'},{'g': 't', 'f': 'y', 'd': 'u', 's':'i'}
+
+database  = client['prv']
+collections = database['prv.1']
+
+
+# data01 = collections.find()
