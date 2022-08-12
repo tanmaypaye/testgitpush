3b07d97ab8cda10e912a94b49dfd105b934f39b2
tanmay <tanmaypaye@gmail.com>
2022-08-12 09:30:49 +0530

    5.1.1

---
+++ page5.py
@@ -0,0 +1,7 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Tanmay:Password@tanmay.si14ffa.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+databese = client['page5']
+collections = databese['page5.1']
+data = {'h1': 'a', 'h2': 'b', 'h3': 'c'}
+
