04b3d6e42991d0c192e00238bbace97f91cd81a6
tanmay <tanmaypaye@gmail.com>
2022-08-18 19:59:24 +0530

    hi

---
+++ dg.py
@@ -0,0 +1,3 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Tanmay:Password@tanmay.si14ffa.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
--- page_1.py
+++ page_1.py
@@ -2,9 +2,16 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://Tanmay:Password@tanmay.si14ffa.mongodb.net/?retryWrites=true&w=majority")
 mydb = client.test
 
-data01 = {'l': 'q', 'k': 'w', 'j': 'e', 'h': 'r'},{'g': 't', 'f': 'y', 'd': 'u', 's':'i'}
+data01 = [{'l': 'q', 'k': 'w', 'j': 'e', 'h': 'r'}, {'g': 't', 'f': 'y', 'd': 'u', 's':'i'}]
 
 database  = client['prv']
-collections = database['prv.11R']
-
-
+collection = database['prv.11R']
+# collection.insert_one(data01)
+# collection.insert_many(data01)
+# collection.update_one({'l': 'q'}, {'$set': {'l': 'z'}})
+# collection.delete_one({'l': 'z'})
+collection.delete_one({'g':'t'})
+d = collection.find({'g' :'t'})
+# collection.find()
+for i in d:
+    print(i)
